018351eb8b501a322409aeebfe0583ea4e8c9067
Jordweinstein <jordanweinstein556@gmail.com>
2023-10-28 15:10:05 -0400

    commit1

---
+++ DyslexiQuiz/DyslexiQuiz App/ProfileView.swift
@@ -0,0 +1,25 @@
+//
+//  ProfileView.swift
+//  DyslexiQuiz
+//
+//  Created by Jordan Weinstein on 10/28/23.
+//
+
+import Foundation
+import SwiftUI
+
+struct ProfileView: View {
+    
+    var body: some View {
+        ZStack{
+            VStack {
+                Image(systemName: "person.circle.fill")
+                    .imageScale(.large)
+            }
+        }
+    }
+}
+#Preview {
+    ProfileView()
+}
+
