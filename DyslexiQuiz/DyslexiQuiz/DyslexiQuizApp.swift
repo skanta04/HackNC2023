d581a0aebcb451385a8d5d2233e4cf85e6671dbf
saishreeya@gmail.com <skanta@ad.unc.edu>
2023-10-28 11:37:54 -0400

    first draft

---
+++ DyslexiQuiz/DyslexiQuiz/DyslexiQuizApp.swift
@@ -0,0 +1,17 @@
+//
+//  DyslexiQuizApp.swift
+//  DyslexiQuiz
+//
+//  Created by Saishreeya Kantamsetty on 10/28/23.
+//
+
+import SwiftUI
+
+@main
+struct DyslexiQuizApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
